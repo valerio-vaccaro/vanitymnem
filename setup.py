5065f1851538e0769a29c74c22163da212dc7cd4
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-05-23 21:45:47 +0200

    update deps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'VanityMnem',
-    version = '0.0.1',
+    version = '0.0.2',
     author = 'Valerio Vaccaro',
     author_email = 'valerio.vaccaro@gmail.com',
     license = 'MIT',
